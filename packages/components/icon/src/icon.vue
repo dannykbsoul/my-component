44de5c8d7eaf4e341642027eb3b7b78f426e73d8
dannykbsoul <346925954@qq.com>
2023-04-02 21:28:51 +0800

    add: prettier

--- packages/components/icon/src/icon.vue
+++ packages/components/icon/src/icon.vue
@@ -5,22 +5,22 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-import { createNamespace } from "@moises/utils/create";
-import { iconProps } from "./icon";
+import { computed } from 'vue'
+import { createNamespace } from '@moises/utils/create'
+import { iconProps } from './icon'
 
 defineOptions({
-  name: "z-icon",
-});
-const bem = createNamespace("icon");
+  name: 'z-icon'
+})
+const bem = createNamespace('icon')
 
-const props = defineProps(iconProps);
+const props = defineProps(iconProps)
 // 计算属性 来计算一个样式来处理
 const style = computed(() => {
-  if (!props.size && !props.color) return {};
+  if (!props.size && !props.color) return {}
   return {
-    ...(props.size ? { "font-size": props.size + "px" } : {}),
-    ...(props.color ? { color: props.color } : {}),
-  };
-});
+    ...(props.size ? { 'font-size': props.size + 'px' } : {}),
+    ...(props.color ? { color: props.color } : {})
+  }
+})
 </script>
